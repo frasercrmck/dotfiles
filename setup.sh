690411e047cab4cc6756d575fc1354469769150e
Fraser <fraser@codeplay.com>
2014-09-10 14:21:47 +0100

    Change permissions on scripts

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -ex
 
+# Vim
+ln -s .vim ~/.vim
 ln -s .vimrc ~/.vimrc
 
+# ZShell
 ln -s .zshrc ~/.zshrc
 ln -s .zshenv ~/.zshenv
-
-ln -s .bashrc ~/.bashrc
-
 ln -s .prompt ~/.prompt
 
+# Bash
+ln -s .bashrc ~/.bashrc
+
+# X11
 ln -s .xinitrc ~/.xinitrc
 ln -s .Xresources ~/.Xresources
 
+# YouCompleteMe
 ln -s .ycm_extra_conf.py ~/.ycm_extra_conf.py
