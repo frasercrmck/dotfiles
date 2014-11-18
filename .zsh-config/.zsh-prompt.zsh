6c328a0f70479fe9dcde58e4b1b5a915cb7f8571
Fraser <fraser@codeplay.com>
2014-11-18 11:18:44 +0000

    Clean up unused zsh prompts

--- .zsh-config/.zsh-prompt.zsh
+++ .zsh-config/.zsh-prompt.zsh
@@ -18,74 +18,12 @@ bmagenta="%{[01;35m%}"
 bcyan="%{[01;36m%}"
 bwhite="%{[01;37m%}"
 
-# underscore (note additive ugrey)
-ublack="%{[04;30m%}"
-ugrey="%{[01;04;30m%}"
-ured="%{[04;31m%}"
-ugreen="%{[04;32m%}"
-uyellow="%{[04;33m%}"
-ublue="%{[04;34m%}"
-umagenta="%{[04;35m%}"
-ucyan="%{[04;36m%}"
-uwhite="%{[04;37m%}"
-
-# blinking
-kgrey="%{[01;05;30m%}"
-kred="%{[05;31m%}"
-kgreen="%{[05;32m%}"
-kyellow="%{[05;33m%}"
-kblue="%{[05;34m%}"
-kmagenta="%{[05;35m%}"
-kcyan="%{[05;36m%}"
-kwhite="%{[05;37m%}"
-
 normal="%{[0m%}"
 
 # example of background colour
 # foreground white = 37
 # background red = 41
 fgwhitebgred="%{[0;37;41m%}"
-
-# simple prompt
-simple_prompt() {
-	PROMPT="$bblue%n@%m(%~)%# $normal"
-}
-
-# raela's prompt
-raela_prompt() {
-	PROMPT="${green}[$normal%n$red@$normal%m $blue($normal%D{%a %b %d %H:%M:%S}$blue) $normal%/$green]$normal%# "
-	RPROMPT=""
-}
-
-# extended prompt
-ext_prompt() {
-	PROMPT="${green}[$bgreen%n$green@$bgreen%m $blue(%D{%a %b %d %H:%M:%S}) $cyan%~$green]$green%#$normal "
-	RPROMPT=""
-}
-
-white_prompt() {
-	colour1=$white
-	colour2=$bwhite
-	PROMPT="$colour1($colour2%n$colour1@$colour2%m$colour1)%#$normal "
-	RPROMPT="$colour1($colour2%~$colour1)$normal"
-}
-
-white_time_left_prompt() {
-	colour1=$white
-	colour2=$bwhite
-	colour3=$grey
-	PROMPT="${colour1}[$colour2%D{%H:%M:%S}$colour1] $colour1($colour2%n$colour1@$colour2%m$colour1)%#$normal "
-	RPROMPT="$colour1($colour2%~$colour1)$normal"
-}
-
-red_time_left_prompt() {
-	colour1=$red
-	colour2=$bred
-	colour3=$grey
-	colour4=$white
-	PROMPT="${colour4}[$colour3%D{%H:%M:%S}$colour4] $colour1($colour2%n$colour1@$colour2%m$colour1)%#$normal "
-	RPROMPT="$colour1($colour2%~$colour1)$normal"
-}
 
 vim_ins_mode="%{$cyan%}[INS]%{$reset_color%}"
 vim_cmd_mode="%{$green%}[CMD]%{$reset_color%}"
@@ -102,9 +40,10 @@ function zle-line-finish {
 }
 zle -N zle-line-finish
 
-# Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode indicator, while in fact you would be in INS mode
-# Fixed by catching SIGINT (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything else depends on it, we will not break it
-# Thanks Ron! (see comments)
+# Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode
+# indicator, while in fact you would be in INS mode Fixed by catching SIGINT
+# (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything
+# else depends on it, we will not break it Thanks Ron! (see comments)
 function TRAPINT() {
   vim_mode=$vim_ins_mode
   return $(( 128 + $1 ))
