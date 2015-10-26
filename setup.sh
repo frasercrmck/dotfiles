f68a283140ec70bb5b7d407d704c0bb27ed3b4ac
Fraser Cormack <fraser@codeplay.com>
2015-10-26 17:48:57 +0000

    Use helpers in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,30 +6,46 @@ echo ${script_dir}
 
 cd ~/
 
-for file in .vimrc .zshrc .zshenv .bashrc .xinitrc .Xresources .ycm_extra_conf.py
-do
-  if [ ! -f ${script_dir}/${file} ]; then
-    echo "File '${script_dir}/${file}' does not exist!"
-    continue
+function symlink_file()
+{
+  local FILE=$1
+  local LINK=$2
+
+  if [ ! -f ${FILE} ]; then
+    echo "File '${FILE}' does not exist!"
+    return 0
   fi
-  if [ -f ${file} ]; then
-    rm ${file} 2> /dev/null
+  if [ -f ${LINK} ]; then
+    rm ${LINK} 2> /dev/null
   fi
   set -x
-  ln -s ${script_dir}/${file} ${file}
+  ln -s ${FILE} ${LINK}
   { set +x; } 2>/dev/null
+}
+
+for file in .vimrc .zshrc .zshenv .bashrc .xinitrc .Xresources .ycm_extra_conf.py
+do
+  symlink_file ${script_dir}/${file} ${file}
 done
+
+function symlink_dir()
+{
+  local DIR=$1
+  local LINK=$2
+
+  if [ ! -d ${DIR} ]; then
+    echo "Directory '${DIR}' does not exist!"
+    return 0
+  fi
+  if [ -d ${LINK} ]; then
+    rm -r ${LINK} 2> /dev/null
+  fi
+  set -x
+  ln -s ${DIR} ${LINK}
+  { set +x; } 2>/dev/null
+}
 
 for dir in .i3 .config/nvim .vim .zsh-config
 do
-  if [ ! -d ${script_dir}/${dir} ]; then
-    echo "Directory '${script_dir}/${dir}' does not exist!"
-    continue
-  fi
-  if [ -d ${dir} ]; then
-    rm -r ${dir} 2> /dev/null
-  fi
-  set -x
-  ln -s ${script_dir}/${dir} ${dir}
-  { set +x; } 2>/dev/null
+  symlink_dir ${script_dir}/${dir} ${dir}
 done
