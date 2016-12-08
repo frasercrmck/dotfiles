773fc90f302022c5a5bcb5949b15fa05d2a93ccd
Fraser Cormack <frasercrmck@gmail.com>
2016-12-08 15:55:38 +0000

    Add more colours

--- i3.xdg_cfg/i3lock.sh
+++ i3.xdg_cfg/i3lock.sh
@@ -6,6 +6,11 @@ COLOURS=(
   67B5D1 # Turquoise
   A4BC62 # Green
   822C6C # Purple
+  555577 # Blue
+  113388 # Blue 2
+  DDAACC # Pinky
+  775577 # Purpley
+  ADD8B8 # Greeny
 )
 
 RANDOM=$$$(date +%s)
