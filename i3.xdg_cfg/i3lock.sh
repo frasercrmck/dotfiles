e65f8b47302fea079a8d932946f1f85a067ce7dc
Fraser Cormack <frasercrmck@gmail.com>
2017-05-15 11:50:48 +0100

    Add some more lock screen colours

--- i3.xdg_cfg/i3lock.sh
+++ i3.xdg_cfg/i3lock.sh
@@ -3,14 +3,18 @@
 COLOURS=(
   DAB84A # Yellow
   DA704A # Red
+  F17A64 # Red2
   67B5D1 # Turquoise
   A4BC62 # Green
   822C6C # Purple
   555577 # Blue
-  113388 # Blue 2
+  63CEE7 # Blue2
   DDAACC # Pinky
   775577 # Purpley
+  AE69E7 # Purpley2
   ADD8B8 # Greeny
+  8FF17B # Greeny2
+  CCF1D4 # Greeny3
 )
 
 RANDOM=$$$(date +%s)
