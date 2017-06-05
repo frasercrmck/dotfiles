7a3f71e222329791921d2008af5fe30fb4f7450f
Fraser Cormack <frasercrmck@gmail.com>
2017-06-05 15:10:21 +0100

    Remove unused zsh colour variables

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,32 +1,5 @@
 autoload -U colors && colors
 
-# normal
-black="%{[0;30m%}"
-red="%{[0;31m%}"
-green="%{[0;32m%}"
-yellow="%{[0;33m%}"
-blue="%{[0;34m%}"
-magenta="%{[0;35m%}"
-cyan="%{[0;36m%}"
-white="%{[0;37m%}"
-
-# bold (grey is actually bold black)
-grey="%{[01;30m%}"
-bred="%{[01;31m%}"
-bgreen="%{[01;32m%}"
-byellow="%{[01;33m%}"
-bblue="%{[01;34m%}"
-bmagenta="%{[01;35m%}"
-bcyan="%{[01;36m%}"
-bwhite="%{[01;37m%}"
-
-normal="%{[0m%}"
-
-# example of background colour
-# foreground white = 37
-# background red = 41
-fgwhitebgred="%{[0;37;41m%}"
-
 vim_ins_mode="%B%F{cyan}[I]%f%b"
 vim_cmd_mode="%B%F{green}[C]%f%b"
 vim_mode=$vim_ins_mode
@@ -53,8 +26,8 @@ function TRAPINT() {
 
 vim_time_prompt() {
   CUR_DIR="%(5~|%-1~/…/%3~|%4~)"
-  PROMPT='[%B%D{%H:%M}%b] ${vim_mode} (%B%n%b)%#${normal} '
-  RPROMPT="(%B${CUR_DIR}%b)${normal}"
+  PROMPT='[%B%D{%H:%M}%b] ${vim_mode} (%B%n%b)%# '
+  RPROMPT="(%B${CUR_DIR}%b)"
   RPROMPT2=${RPROMPT}
 }
 
