1958cafcc2f8d623142dad1aa56ef096c131becd
Fraser Cormack <frasercrmck@gmail.com>
2019-02-26 09:45:03 +0000

    Improve i3lock colours

--- i3.xdg_cfg/scripts/i3lock.sh
+++ i3.xdg_cfg/scripts/i3lock.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 COLOURS=(
-  DAB84A # Yellow
-  DA704A # Red
-  F17A64 # Red2
-  67B5D1 # Turquoise
-  A4BC62 # Green
-  822C6C # Purple
-  555577 # Blue
-  63CEE7 # Blue2
-  DDAACC # Pinky
-  775577 # Purpley
-  AE69E7 # Purpley2
-  ADD8B8 # Greeny
-  8FF17B # Greeny2
-  CCF1D4 # Greeny3
+  F8B195 # Tangerine
+  F67280 # Light red
+  C06C84 # Sunset
+  6C5B7B # Purple
+  C55C7D # Dark blue
+  A8E6CE # Turqouiseish
+  DCEDC2 # Lime-Greenish
+  FFD3B5 # Orangeish
+  FFAAA6 # Pinkish
+  FF8C94 # Reddish
+  EFDD6F # Yellow
 )
 
 RANDOM=$$$(date +%s)
