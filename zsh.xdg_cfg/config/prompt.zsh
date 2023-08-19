3acbac417a8130bebf1456e9402b0b75f29795f1
Fraser Cormack <frasercrmck@gmail.com>
2023-08-19 09:19:02 +0100

    [zsh] Work on the prompt a little more

--- zsh.xdg_cfg/config/prompt.zsh
+++ zsh.xdg_cfg/config/prompt.zsh
@@ -3,28 +3,31 @@ autoload -U colors && colors
 # Remove delay in mode switching
 KEYTIMEOUT=5
 
+_set_block_cursor() {
+  echo -ne '\e[2 q'
+}
+_set_beam_cursor() {
+  echo -ne '\e[5 q'
+}
+
 function zle-keymap-select {
   if [ ${KEYMAP} = "vicmd" ]; then
-    echo -ne '\e[1 q'
+    _set_block_cursor
   elif [[ ${KEYMAP} =~ "main|viins" ]]; then
-    echo -ne '\e[5 q'
+    _set_beam_cursor
   fi
 }
 
 zle -N zle-keymap-select
 
-_fix_cursor() {
-  echo -ne '\e[5 q'
-}
-
-precmd_functions+=(_fix_cursor)
+precmd_functions+=(_set_beam_cursor)
 
 # Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode
 # indicator, while in fact you would be in INS mode Fixed by catching SIGINT
 # (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything
 # else depends on it, we will not break it Thanks Ron! (see comments)
 function TRAPINT() {
-  _fix_cursor
+  _set_beam_cursor
   return $(( 128 + $1 ))
 }
 
@@ -34,7 +37,7 @@ vim_time_prompt() {
   if [[ -n "$SSH_CLIENT" ]]; then
     HNAME="·%B%F{red}%m%f%b"
   fi
-  PROMPT='[%B%F{cyan}%n%f%b@%B%F{green}%m%f%b]${HNAME}·%# '
+  PROMPT='%B[%F{yellow}%n%f%F{green}@%f%F{blue}%m%f]%b${HNAME}%# '
   RPROMPT="(%B${CUR_DIR}%b)"
   RPROMPT2=${RPROMPT}
 }
