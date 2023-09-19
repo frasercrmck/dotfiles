2efab6de9799dc12f23f31e5fdbb6d5fcbc25596
Fraser Cormack <frasercrmck@gmail.com>
2023-09-19 17:54:16 +0100

    [zsh] Add dynamic bookmarking

--- xdg_cfg/zsh/config/prompt.zsh
+++ xdg_cfg/zsh/config/prompt.zsh
@@ -10,6 +10,55 @@ _set_beam_cursor() {
   echo -ne '\e[5 q'
 }
 
+# Taken from https://github.com/vincentbernat/zshrc/blob/master/rc/bookmarks.zsh
+# Bookmarks are symlinks stored in $XDG_DATA_HOME/markpaths, e.g.,
+#   woof -> /home/fraser/woof
+# Creates a dynamic directory name ~[@woof] which points to /home/fraser/woof
+zsh_directory_name() {
+  emulate -L zsh
+  setopt extendedglob
+  MARKPATH=$XDG_DATA_HOME/markpaths
+  case $1 in
+    n)
+      [[ $2 != (#b)"@"(?*) ]] && return 1
+      typeset -ga reply
+      reply=(${${:-$MARKPATH/$match[1]}:A})
+      return 0
+      ;;
+    d)
+      local link slink split_slink
+      local -A links
+      for link ($MARKPATH/*(N@)) {
+        links[${#link:A}$'\0'${link:A}]=${link:t}
+      }
+      for slink (${(@On)${(k)links}}) {
+        #link=${slink#*$'\0'}
+        split_slink=("${(@s/'\0'/)slink}")
+        link=$split_slink[2]
+        if [[ $2 = (#b)(${link})(|/*) ]]; then
+          typeset -ga reply
+          reply=("@"${links[$slink]} $(( ${#match[1]} )) )
+          return 0
+        fi
+      }
+      return 1
+      ;;
+    c)
+      # Completion
+      local expl
+      local -a dirs
+      dirs=($MARKPATH/*(N@:t))
+      dirs=("@"${^dirs})
+      _wanted dynamic-dirs expl 'bookmarked directory' compadd -S\] -a dirs
+      return
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+  return 0
+}
+
 function zle-keymap-select {
   if [ ${KEYMAP} = "vicmd" ]; then
     _set_block_cursor
