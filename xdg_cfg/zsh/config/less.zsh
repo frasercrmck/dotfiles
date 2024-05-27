2827e4f0c595762832d37b058381acaa7f7b898c
Fraser Cormack <frasercrmck@gmail.com>
2024-05-27 12:18:29 +0100

    [zsh] Export less colours

---
+++ xdg_cfg/zsh/config/less.zsh
@@ -0,0 +1,8 @@
+# termcap config for less, man, etc.
+export LESS_TERMCAP_mb=`printf "\e[0;31m"`
+export LESS_TERMCAP_md=`printf "\e[0;36m"`
+export LESS_TERMCAP_me=`printf "\e[0m"`
+export LESS_TERMCAP_so=`printf "\e[1;40;32m"`
+export LESS_TERMCAP_se=`printf "\e[0m"`
+export LESS_TERMCAP_us=`printf "\e[0;34m"`
+export LESS_TERMCAP_ue=`printf "\e[0m"`
